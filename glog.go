62e1a766bfeeeeeae9c22d7cbac56f662f9b8dc4
Andy Dunstall <andydunstall@hotmail.com>
2020-08-02 03:15:34 -0400

    Replace flags with Configure

--- glog.go
+++ glog.go
@@ -396,18 +396,21 @@ type flushSyncWriter interface {
 }
 
 func init() {
-	flag.BoolVar(&logging.toStderr, "logtostderr", false, "log to standard error instead of files")
-	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr", false, "log to standard error as well as files")
-	flag.Var(&logging.verbosity, "v", "log level for V logs")
-	flag.Var(&logging.stderrThreshold, "stderrthreshold", "logs at or above this threshold go to stderr")
-	flag.Var(&logging.vmodule, "vmodule", "comma-separated list of pattern=N settings for file-filtered logging")
-	flag.Var(&logging.traceLocation, "log_backtrace_at", "when logging hits line file:N, emit a stack trace")
-
 	// Default stderrThreshold is ERROR.
 	logging.stderrThreshold = errorLog
 
 	logging.setVState(0, nil, false)
 	go logging.flushDaemon()
+}
+
+func Configure(verbose bool, threshold severity) {
+	logging.toStderr = true
+	logging.alsoToStderr = false
+
+	logging.stderrThreshold = threshold
+	if verbose {
+		logging.verbosity.set(1)
+	}
 }
 
 // Flush flushes all pending log I/O.
